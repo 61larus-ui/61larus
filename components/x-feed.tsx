34b31cc26904e786037f359933ca00cd1d560a87
Ceyhun <test1@gmail.com>
2026-04-26 19:25:58 +0300

    Add tweet embed

--- components/x-feed.tsx
+++ components/x-feed.tsx
@@ -4,39 +4,36 @@ import { useEffect } from "react";
 
 export default function XFeed() {
   useEffect(() => {
-    const existingScript = document.getElementById("twitter-script");
-
-    if (!existingScript) {
+    if (!document.getElementById("twitter-script")) {
       const script = document.createElement("script");
       script.id = "twitter-script";
       script.src = "https://platform.twitter.com/widgets.js";
       script.async = true;
-      script.onload = () => {
-        // @ts-ignore
-        if (window.twttr) {
-          // @ts-ignore
-          window.twttr.widgets.load();
-        }
-      };
       document.body.appendChild(script);
-    } else {
-      // @ts-ignore
-      if (window.twttr) {
-        // @ts-ignore
-        window.twttr.widgets.load();
-      }
     }
   }, []);
 
   return (
     <div style={{ marginTop: "24px" }}>
+      <p style={{ fontSize: "14px", opacity: 0.6 }}>
+        61Larus X paylaşımları
+      </p>
+
+      <blockquote className="twitter-tweet">
+        <a href="https://x.com/6Larus/status/2048436993718157792"></a>
+      </blockquote>
+
       <a
-        className="twitter-timeline"
-        data-height="500"
-        data-theme="light"
-        href="https://twitter.com/61larus"
+        href="https://x.com/6Larus"
+        target="_blank"
+        style={{
+          display: "inline-block",
+          marginTop: "12px",
+          fontSize: "14px",
+          textDecoration: "underline"
+        }}
       >
-        X akışı yükleniyor...
+        {"X\u2019te takip et →"}
       </a>
     </div>
   );
